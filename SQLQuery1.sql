f6d63161bad5b12b767ce6e2351967bdd2ee5e5a
Akash Kumar Sengar <akashsengar55@gmail.com>
2022-03-25 07:03:30 +0530

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿Create database DataDrop
+Use DataDrop
+
+
+insert into Countries values
+('India'),('USA'),('Japan')
+
+select * from Countries;
+select * from States
+select * from Cities
+insert into States values
+('Up',1),('Maharashtra',1),('California',2),('Washington',2),('Kanto',3)
+
+insert into Cities values
+     ('LUcknow',1),('Kanpur',1),('Mumbai',2),('Los Angeles',3),('Tokyo',5)
